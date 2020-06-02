94d81aef5f3754f99e66630968d0c1ac86fa8115
Azin Shamshirgaran <36013491+azinsh1988@users.noreply.github.com>
2020-06-02 00:33:49 -0400

    Create stringPlay.py

---
+++ SimpleQuestions/stringPlay.py
@@ -0,0 +1,14 @@
+#In the first line, print True if  has any alphanumeric characters. Otherwise, print False.
+#In the second line, print True if  has any alphabetical characters. Otherwise, print False.
+#In the third line, print True if  has any digits. Otherwise, print False.
+#In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
+#In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
+
+
+if __name__ == '__main__':
+  S = input()
+  print (any([char.isalnum() for char in S]))
+  print (any([char.isalpha() for char in S]))
+  print (any([char.isdigit() for char in S]))
+  print (any([char.islower() for char in S]))
+  print (any([char.isupper() for char in S]))
