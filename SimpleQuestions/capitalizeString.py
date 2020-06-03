3eda7a647a4ca052cdbbc248c0bce9b3580152a1
Azin Shamshirgaran <36013491+azinsh1988@users.noreply.github.com>
2020-06-02 20:47:33 -0400

    Update capitalizeString.py

--- SimpleQuestions/capitalizeString.py
+++ SimpleQuestions/capitalizeString.py
@@ -1,25 +1,13 @@
 #Given a full name, your task is to capitalize the name appropriately.
 
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the solve function below.
 def solve(s):
     for x in s[:].split():
         s = s.replace(x, x.capitalize())
     return s
+
+
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     s = input()
-
     result = solve(s)
-
-    fptr.write(result + '\n')
-
-    fptr.close()
+    print(result)
